94244d389e8f4c944c2c2cb6d75349426e970ddb
Arkhorse <theillusivemman@gmail.com>
2023-07-13 17:55:44 -0300

    Create GodrayManager_Update.cs

---
+++ VisualStudio/Patches/GodrayManager_Update.cs
@@ -0,0 +1,16 @@
+﻿namespace ExtraGraphicsSettings
+{
+    [HarmonyPatch(typeof(GodrayManager), nameof(GodrayManager.Update))]
+    public class GodrayManager_Update
+    {
+        public static void Postfix(GodrayManager __instance)
+        {
+            bool FirstStart = true;
+            if (!FirstStart || !__instance.m_Initialized) return;
+
+            Settings.DefaultGodraysColour = GameManager.GetUniStorm().GetActiveTODState().m_GodrayColor;
+
+            FirstStart = false;
+        }
+    }
+}
