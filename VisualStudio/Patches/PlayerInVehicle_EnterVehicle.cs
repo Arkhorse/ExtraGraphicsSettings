743afb9013a031e5714e8a1d872627f9822e38d0
Arkhorse <theillusivemman@gmail.com>
2023-07-12 21:55:37 -0300

    merge mod FreeLookInCars

---
+++ VisualStudio/Patches/PlayerInVehicle_EnterVehicle.cs
@@ -0,0 +1,15 @@
+﻿namespace ExtraGraphicsSettings
+{
+    [HarmonyPatch(typeof(PlayerInVehicle), nameof(PlayerInVehicle.EnterVehicle))]
+    internal class PlayerInVehicle_EnterVehicle
+    {
+        private static void Postfix(PlayerInVehicle __instance)
+        {
+            if (Settings.Instance.EnableFreeLook)
+            {
+                __instance.m_YawLimitDegrees = new Vector2(-Settings.Instance.FreeLook_MaxYaw, Settings.Instance.FreeLook_MaxYaw);
+                __instance.m_PitchLimitDegrees = new Vector2(-Settings.Instance.FreeLook_MaxPitch, Settings.Instance.FreeLook_MaxPitch);
+            }
+        }
+    }
+}
