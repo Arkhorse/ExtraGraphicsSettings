d45202a24824b6eb86a1e2d534112e218b540672
Arkhorse <theillusivemman@gmail.com>
2023-07-13 17:55:40 -0300

    Update GodraysUpdater.cs

--- VisualStudio/Utilities/GodraysUpdater.cs
+++ VisualStudio/Utilities/GodraysUpdater.cs
@@ -9,19 +9,6 @@
         {
             UniStormWeatherSystem unistorm = GameManager.GetUniStorm();
 
-            if (Settings.Instance.Godrays == GodraysPresets.On)
-            {
-                Settings.GodraysColor = Color.magenta;
-            }
-            else if (Settings.Instance.Godrays == GodraysPresets.Off)
-            {
-                Settings.GodraysColor = new Color(0f, 0f, 0f);
-            }
-            else if (Settings.Instance.Godrays == GodraysPresets.Custom)
-            {
-                Settings.GodraysColor = new Color(Settings.Instance.GodraysColorRed, Settings.Instance.GodraysColorGreen, Settings.Instance.GodraysColorBlue);
-            }
-
             foreach (WeatherStatesData eachWeatherStateConfig in unistorm.m_WeatherStateConfigs)
             {
                 TODStateUtils.ChangeTODGodrays(eachWeatherStateConfig.m_DawnColors,         color: Settings.GodraysColor);
