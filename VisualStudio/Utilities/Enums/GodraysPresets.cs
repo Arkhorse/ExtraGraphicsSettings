2fac0479d76a60b3d215004d8a42e3030d66664c
Arkhorse <theillusivemman@gmail.com>
2023-07-12 21:53:17 -0300

    Create GodraysPresets.cs

---
+++ VisualStudio/Utilities/Enums/GodraysPresets.cs
@@ -0,0 +1,10 @@
+﻿namespace ExtraGraphicsSettings
+{
+    /// <summary>
+    /// Used with the Settings class to setup Godrays options
+    /// </summary>
+    public enum GodraysPresets
+    {
+        On, Off, Presets, Custom
+    }
+}
