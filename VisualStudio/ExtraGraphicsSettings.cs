2413ce10ee0976745d19726025c09755b1b2ae91
Arkhorse <theillusivemman@gmail.com>
2023-07-13 17:55:14 -0300

    Update ExtraGraphicsSettings.cs

--- VisualStudio/ExtraGraphicsSettings.cs
+++ VisualStudio/ExtraGraphicsSettings.cs
@@ -1,13 +1,24 @@
 namespace ExtraGraphicsSettings
 {
-    public class ExtraGraphicsSettings : MelonMod
+    public class Main : MelonMod
     {
         public override void OnInitializeMelon()
         {
             Settings.OnLoad();
-#if DEBUG
-            Logger.Log($"Mod has loaded with version: {BuildInfo.Version}");
-#endif
+        }
+
+        public override void OnSceneWasLoaded(int buildIndex, string sceneName)
+        {
+            if (sceneName.Contains("SANDBOX"))
+            {
+                if (!GameManager.GetUniStorm())
+                {
+                    Logger.LogWarning("Game is not loaded yet or user has yet to load a save", Color.yellow);
+                    return;
+                }
+                Settings.DefaultGodraysColour = GameManager.GetUniStorm().GetActiveTODState().m_GodrayColor;
+            }
+            base.OnSceneWasLoaded(buildIndex, sceneName);
         }
     }
 }
