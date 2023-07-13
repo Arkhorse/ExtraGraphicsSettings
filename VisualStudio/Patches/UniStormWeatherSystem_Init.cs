f9af8b567f5a404b8d4cd9f70c18888ec43bcbfb
Arkhorse <theillusivemman@gmail.com>
2023-07-12 21:55:56 -0300

    Merge mod DisableVignette

---
+++ VisualStudio/Patches/UniStormWeatherSystem_Init.cs
@@ -0,0 +1,44 @@
+﻿namespace ExtraGraphicsSettings
+{
+    [HarmonyPatch(typeof(UniStormWeatherSystem), nameof(UniStormWeatherSystem.Init))]
+    internal class UniStormWeatherSystem_Init
+    {
+        private static void Postfix(UniStormWeatherSystem __instance)
+        {
+            if (!Settings.Instance.Vignette)
+            {
+                foreach (WeatherStatesData eachWeatherStateConfig in __instance.m_WeatherStateConfigs)
+                {
+                    if (eachWeatherStateConfig == null)
+                    {
+                        continue;
+                    }
+                    TODStateUtils.ChangeTODStateData(eachWeatherStateConfig.m_DawnColors, "vignette");
+                    TODStateUtils.ChangeTODStateData(eachWeatherStateConfig.m_MorningColors, "vignette");
+                    TODStateUtils.ChangeTODStateData(eachWeatherStateConfig.m_MiddayColors, "vignette");
+                    TODStateUtils.ChangeTODStateData(eachWeatherStateConfig.m_AfternoonColors, "vignette");
+                    TODStateUtils.ChangeTODStateData(eachWeatherStateConfig.m_DuskColors, "vignette");
+                    TODStateUtils.ChangeTODStateData(eachWeatherStateConfig.m_NightColors, "vignette");
+                }
+            }
+            if (!Settings.Instance.Chromatic)
+            {
+                foreach (WeatherStatesData eachWeatherStateConfig in __instance.m_WeatherStateConfigs)
+                {
+                    if (eachWeatherStateConfig == null)
+                    {
+                        continue;
+                    }
+                    TODStateUtils.ChangeTODStateData(eachWeatherStateConfig.m_DawnColors, "chromatic");
+                    TODStateUtils.ChangeTODStateData(eachWeatherStateConfig.m_MorningColors, "chromatic");
+                    TODStateUtils.ChangeTODStateData(eachWeatherStateConfig.m_MiddayColors, "chromatic");
+                    TODStateUtils.ChangeTODStateData(eachWeatherStateConfig.m_AfternoonColors, "chromatic");
+                    TODStateUtils.ChangeTODStateData(eachWeatherStateConfig.m_DuskColors, "chromatic");
+                    TODStateUtils.ChangeTODStateData(eachWeatherStateConfig.m_NightColors, "chromatic");
+                }
+            }
+
+            GodraysUpdater.UpdateGodraysColor();
+        }
+    }
+}
---
+++ VisualStudio/Patches/UniStormWeatherSystem_InitForScene.cs
@@ -0,0 +1,47 @@
+﻿namespace ExtraGraphicsSettings
+{
+    [HarmonyPatch(typeof(UniStormWeatherSystem), nameof(UniStormWeatherSystem.InitializeForScene))]
+    internal class UniStormWeatherSystem_InitForScene
+    {
+        public static void Postfix(UniStormWeatherSystem __instance)
+        {
+            if (__instance.m_Indoors) return;
+
+            if (!Settings.Instance.Vignette)
+            {
+                foreach (WeatherStatesData eachWeatherStateConfig in __instance.m_WeatherStateConfigs)
+                {
+                    if (eachWeatherStateConfig == null)
+                    {
+                        continue;
+                    }
+                    TODStateUtils.ChangeTODStateData(eachWeatherStateConfig.m_DawnColors,       "vignette");
+                    TODStateUtils.ChangeTODStateData(eachWeatherStateConfig.m_MorningColors,    "vignette");
+                    TODStateUtils.ChangeTODStateData(eachWeatherStateConfig.m_MiddayColors,     "vignette");
+                    TODStateUtils.ChangeTODStateData(eachWeatherStateConfig.m_AfternoonColors,  "vignette");
+                    TODStateUtils.ChangeTODStateData(eachWeatherStateConfig.m_DuskColors,       "vignette");
+                    TODStateUtils.ChangeTODStateData(eachWeatherStateConfig.m_NightColors,      "vignette");
+                }
+            }
+
+            if (!Settings.Instance.Chromatic)
+            {
+                foreach (WeatherStatesData eachWeatherStateConfig in __instance.m_WeatherStateConfigs)
+                {
+                    if (eachWeatherStateConfig == null)
+                    {
+                        continue;
+                    }
+                    TODStateUtils.ChangeTODStateData(eachWeatherStateConfig.m_DawnColors,       "chromatic");
+                    TODStateUtils.ChangeTODStateData(eachWeatherStateConfig.m_MorningColors,    "chromatic");
+                    TODStateUtils.ChangeTODStateData(eachWeatherStateConfig.m_MiddayColors,     "chromatic");
+                    TODStateUtils.ChangeTODStateData(eachWeatherStateConfig.m_AfternoonColors,  "chromatic");
+                    TODStateUtils.ChangeTODStateData(eachWeatherStateConfig.m_DuskColors,       "chromatic");
+                    TODStateUtils.ChangeTODStateData(eachWeatherStateConfig.m_NightColors,      "chromatic");
+                }
+            }
+
+            GodraysUpdater.UpdateGodraysColor();
+        }
+    }
+}
