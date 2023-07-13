cd555b429fb872142858514f35e261c26b84df0d
Arkhorse <theillusivemman@gmail.com>
2023-07-12 21:53:51 -0300

    Create ConsoleCommands.cs

---
+++ VisualStudio/Utilities/ConsoleCommands.cs
@@ -0,0 +1,16 @@
+namespace TEMPLATE
+{
+    public class ConsoleCommands
+    {
+        // use this to print your debug info, instead of having log spam all the time
+        public static void PrintDebugInfo()
+        {
+
+        }
+
+        public void RegisterCommands()
+        {
+            
+        }
+    }
+}
