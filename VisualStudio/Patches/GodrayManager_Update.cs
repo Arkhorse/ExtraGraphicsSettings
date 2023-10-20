431a840877cadadf1d853b66d216db5b5b321470
The Illusion <20694721+Arkhorse@users.noreply.github.com>
2023-10-20 20:37:40 -0300

    make godrays start/stop

--- VisualStudio/Patches/GodrayManager_Update.cs
+++ VisualStudio/Patches/GodrayManager_Update.cs
@@ -5,12 +5,18 @@
     {
         public static void Postfix(GodrayManager __instance)
         {
-            bool FirstStart = true;
-            if (!FirstStart || !__instance.m_Initialized) return;
+            if (!__instance.m_Initialized) return;
 
             Settings.DefaultGodraysColour = GameManager.GetUniStorm().GetActiveTODState().m_GodrayColor;
 
-            FirstStart = false;
+            if (Settings.Instance.Godrays != GodraysPresets.Off)
+            {
+                __instance.StartRays();
+            }
+            else
+            {
+                __instance.StopRays();
+            }
         }
     }
 }
