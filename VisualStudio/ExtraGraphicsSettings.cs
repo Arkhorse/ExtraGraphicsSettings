2fced79b40222650ff7c2f4585795b1efa917999
The Illusion <20694721+Arkhorse@users.noreply.github.com>
2023-10-31 12:38:37 -0300

    Update ExtraGraphicsSettings.cs

--- VisualStudio/ExtraGraphicsSettings.cs
+++ VisualStudio/ExtraGraphicsSettings.cs
@@ -1,4 +1,5 @@
 using ExtraGraphicsSettings.Utilities;
+using ExtraGraphicsSettings.Utilities.Exceptions;
 
 namespace ExtraGraphicsSettings
 {
@@ -9,19 +10,51 @@ namespace ExtraGraphicsSettings
             Settings.OnLoad();
         }
 
-        public override void OnSceneWasInitialized(int buildIndex, string sceneName)
-        {
-            base.OnSceneWasInitialized(buildIndex, sceneName);
+        //public override void OnSceneWasInitialized(int buildIndex, string sceneName)
+        //{
+        //    base.OnSceneWasInitialized(buildIndex, sceneName);
 
-            if (SceneUtilities.IsScenePlayable(sceneName))
-            {
-                if (GameManager.GetUniStorm())
-                {
-                    //TODO: Convert this to json data
-                    Settings.DefaultGodraysColour = GameManager.GetUniStorm().GetActiveTODState().m_GodrayColor;
-                    Settings.OnConfirmLoad();
-                }
-            }
-        }
+        //    if (SceneUtilities.IsSceneEmpty(sceneName) || SceneUtilities.IsSceneBoot(sceneName)) return;
+
+        //    try
+        //    {
+        //        if (GameManager.GetUniStorm() == null) return;
+        //    }
+        //    catch { }
+
+        //    try
+        //    {
+        //        if (SceneUtilities.IsScenePlayable(sceneName))
+        //        {
+        //            //TODO: Convert this to json data
+        //            if (Settings.DefaultGodraysColour == new Color(0, 0, 0)) Settings.DefaultGodraysColour = GameManager.GetUniStorm().GetActiveTODState().m_GodrayColor;
+        //            Settings.Instance.OnConfirmLoad();
+        //        }
+        //    }
+        //    catch { }
+        //}
+
+        //public override void OnLateUpdate()
+        //{
+        //    if (!SceneUtilities.IsSceneBase()) return;
+
+        //    try
+        //    {
+        //        if (GameManager.GetUniStorm() == null) return;
+        //    }
+        //    catch { }
+
+        //    try
+        //    {
+        //        if (GameManager.GetUniStorm() != null)
+        //        {
+
+        //        }
+        //    }
+        //    catch //(Exception e)
+        //    {
+        //        //throw new BadMemeException($"Unistorm is not available. This is OK on game start. Reason {e.Message}");
+        //    }
+        //}
     }
 }
