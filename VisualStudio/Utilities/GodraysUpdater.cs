d487d8b3faa2a66cbaf9f88e7189e177f7062bc3
The Illusion <20694721+Arkhorse@users.noreply.github.com>
2023-10-31 12:38:20 -0300

    Update GodraysUpdater.cs

--- VisualStudio/Utilities/GodraysUpdater.cs
+++ VisualStudio/Utilities/GodraysUpdater.cs
@@ -7,16 +7,24 @@
         /// </summary>
         public static void UpdateGodraysColor()
         {
+            if (GameManager.GetUniStorm() == null) return;
+
             UniStormWeatherSystem unistorm = GameManager.GetUniStorm();
+            Color GodraysColor = Settings.Instance.GodraysColor;
+
+            if (Settings.Instance.GodraysNightAurora)
+            {
+                GodraysColor = GameManager.GetAuroraManager().GetAuroraColour();
+            }
 
             foreach (WeatherStatesData eachWeatherStateConfig in unistorm.m_WeatherStateConfigs)
             {
-                TODStateUtils.ChangeTODGodrays(eachWeatherStateConfig.m_DawnColors,         color: Settings.GodraysColor);
-                TODStateUtils.ChangeTODGodrays(eachWeatherStateConfig.m_MorningColors,      color: Settings.GodraysColor);
-                TODStateUtils.ChangeTODGodrays(eachWeatherStateConfig.m_MiddayColors,       color: Settings.GodraysColor);
-                TODStateUtils.ChangeTODGodrays(eachWeatherStateConfig.m_AfternoonColors,    color: Settings.GodraysColor);
-                TODStateUtils.ChangeTODGodrays(eachWeatherStateConfig.m_DuskColors,         color: Settings.GodraysColor);
-                TODStateUtils.ChangeTODGodrays(eachWeatherStateConfig.m_NightColors,        color: Settings.GodraysColor);
+                TODStateUtils.ChangeTODGodrays(eachWeatherStateConfig.m_DawnColors,         color: GodraysColor);
+                TODStateUtils.ChangeTODGodrays(eachWeatherStateConfig.m_MorningColors,      color: GodraysColor);
+                TODStateUtils.ChangeTODGodrays(eachWeatherStateConfig.m_MiddayColors,       color: GodraysColor);
+                TODStateUtils.ChangeTODGodrays(eachWeatherStateConfig.m_AfternoonColors,    color: GodraysColor);
+                TODStateUtils.ChangeTODGodrays(eachWeatherStateConfig.m_DuskColors,         color: GodraysColor);
+                TODStateUtils.ChangeTODGodrays(eachWeatherStateConfig.m_NightColors,        color: GodraysColor);
             }
         }
     }
