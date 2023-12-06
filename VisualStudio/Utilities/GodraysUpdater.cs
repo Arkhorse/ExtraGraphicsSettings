d5070c321e111502e21eece81969dfbff3515f48
The Illusion <20694721+Arkhorse@users.noreply.github.com>
2023-12-05 20:32:43 -0400

    Update GodraysUpdater.cs
    removed the indoor check, dont actually need that

--- VisualStudio/Utilities/GodraysUpdater.cs
+++ VisualStudio/Utilities/GodraysUpdater.cs
@@ -10,22 +10,35 @@
             if (GameManager.GetUniStorm() == null) return;
 
             UniStormWeatherSystem unistorm = GameManager.GetUniStorm();
-            Color GodraysColor = Settings.Instance.GodraysColor;
-
-            if (Settings.Instance.GodraysNightAurora)
-            {
-                GodraysColor = GameManager.GetAuroraManager().GetAuroraColour();
-            }
 
             foreach (WeatherStatesData eachWeatherStateConfig in unistorm.m_WeatherStateConfigs)
             {
-                TODStateUtils.ChangeTODGodrays(eachWeatherStateConfig.m_DawnColors,         color: GodraysColor);
-                TODStateUtils.ChangeTODGodrays(eachWeatherStateConfig.m_MorningColors,      color: GodraysColor);
-                TODStateUtils.ChangeTODGodrays(eachWeatherStateConfig.m_MiddayColors,       color: GodraysColor);
-                TODStateUtils.ChangeTODGodrays(eachWeatherStateConfig.m_AfternoonColors,    color: GodraysColor);
-                TODStateUtils.ChangeTODGodrays(eachWeatherStateConfig.m_DuskColors,         color: GodraysColor);
-                TODStateUtils.ChangeTODGodrays(eachWeatherStateConfig.m_NightColors,        color: GodraysColor);
+                TODStateUtils.ChangeTODGodrays(eachWeatherStateConfig.m_DawnColors,         color: GetGodraysColor());
+                TODStateUtils.ChangeTODGodrays(eachWeatherStateConfig.m_MorningColors,      color: GetGodraysColor());
+                TODStateUtils.ChangeTODGodrays(eachWeatherStateConfig.m_MiddayColors,       color: GetGodraysColor());
+                TODStateUtils.ChangeTODGodrays(eachWeatherStateConfig.m_AfternoonColors,    color: GetGodraysColor());
+                TODStateUtils.ChangeTODGodrays(eachWeatherStateConfig.m_DuskColors,         color: GetGodraysColor());
+                TODStateUtils.ChangeTODGodrays(eachWeatherStateConfig.m_NightColors,        color: GetGodraysColor());
             }
+        }
+
+        public static Color GetGodraysColor()
+        {
+            Color GodraysColor = new(1,1,1,0);
+            if (GameManager.GetUniStorm() != null)
+            {
+                UniStormWeatherSystem unistorm = GameManager.GetUniStorm();
+                GodraysColor = Settings.Instance.GodraysColor;
+
+                if (Settings.Instance.GodraysNightAurora)
+                {
+                    GodraysColor = GameManager.GetAuroraManager().GetAuroraColour();
+                }
+
+                GodraysColor.a = (Settings.Instance.Godrays == GodraysPresets.Custom) ? Settings.Instance.GodraysColorAlpha : 1;
+            }
+
+            return GodraysColor;
         }
     }
 }
