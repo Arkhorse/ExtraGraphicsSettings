4a1fc307d697bbc5396d68243bfc4b787047e995
The Illusion <20694721+Arkhorse@users.noreply.github.com>
2023-12-05 20:34:41 -0400

    1.1.4

--- VisualStudio/BuildInfo.cs
+++ VisualStudio/BuildInfo.cs
@@ -8,7 +8,7 @@
         /// <summary>Who made the mod</summary>
         public const string Author                          = "The Illusion";
         /// <summary>Current version (Using Major.Minor.Build) </summary>
-        public const string Version                         = "1.1.3";
+        public const string Version                         = "1.1.4";
         /// <summary>Name used on GUI's, like ModSettings</summary>
         public const string GUIName                         = "Extra Graphics Settings";
         /// <summary>The minimum Melon Loader version that your mod requires</summary>
